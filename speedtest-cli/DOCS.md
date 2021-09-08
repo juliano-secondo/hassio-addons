66040141c415d3619625518196eedd9b0587ebfd
Juliano Secondo <juliano.secondo@kumulus.com.br>
2021-09-08 16:43:20 -0300

    SpeedTestCli v0 commit

---
+++ speedtest-cli/DOCS.md
@@ -0,0 +1,17 @@
+## Configuration
+
+**Note**: _Remember to restart the add-on when the configuration is changed._
+
+Example add-on configuration:
+
+```yaml
+servers:
+  - 22460
+  - 17484
+  - 22249
+crons:
+  - '*/20 * * * *'
+```
+
+**Note**: _This is just an example, don't copy and paste it! Create your own!_
+
---
+++ speedtest-cli/rootfs/speedtestcli.sh
@@ -0,0 +1,32 @@
+if [ "$SUPERVISOR_TOKEN" = "" ];
+then
+    echo "$(date) - Error token is empty"
+    exit 2
+fi
+
+for SERVER in $SERVERS;
+do
+    echo "$(date) - Starting speedtest server ${SERVER}"
+    JSON=`speedtest -s ${SERVER} --f json-pretty`
+    if [ $? = 0 ];
+    then
+        echo "$(date) - Finished speedtest"
+        ATTRIBUTES="{\"unit_of_measurement\": \"Mbit/s\", \"state_class\": \"measurement\", \"icon\": \"mdi:speedometer\"}"
+        DOWNLOADVALUE=`echo $JSON | jq '.download.bandwidth'`
+        DOWNLOADVALUEFINAL=`expr $DOWNLOADVALUE \* 8 / 1024 / 1024`
+        UPLOADVALUE=`echo $JSON | jq '.upload.bandwidth'`
+        UPLOADVALUEFINAL=`expr $UPLOADVALUE \* 8 / 1024 / 1024`
+        DOWNLOAD="{\"state\": \"${DOWNLOADVALUEFINAL}\", \"attributes\": ${ATTRIBUTES} }"
+        UPLOAD="{\"state\": \"${UPLOADVALUEFINAL}\", \"attributes\": ${ATTRIBUTES} }"
+        PING="{\"state\": \"`echo $JSON | jq '.ping.latency'`\", \"attributes\": ${ATTRIBUTES} }"
+        echo "$(date) - DL: $DOWNLOADVALUE, UP: $UPLOADVALUE, PING: $(echo $JSON | jq '.ping.latency')"
+
+        curl -s -X POST -H "Authorization: Bearer ${SUPERVISOR_TOKEN}" -H "Content-Type: application/json" http://supervisor/core/api/states/sensor.speedtestcli_download -d "${DOWNLOAD}"
+        curl -s -X POST -H "Authorization: Bearer ${SUPERVISOR_TOKEN}" -H "Content-Type: application/json" http://supervisor/core/api/states/sensor.speedtestcli_upload -d "${UPLOAD}"
+        curl -s -X POST -H "Authorization: Bearer ${SUPERVISOR_TOKEN}" -H "Content-Type: application/json" http://supervisor/core/api/states/sensor.speedtestcli_ping -d "${PING}"
+        exit 0
+    else
+        echo "$(date) - Error connect server ${SERVER}, trying other..."
+    fi
+done
+
